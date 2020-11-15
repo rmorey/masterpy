63f78763bc9505c1117c4f88a2438b076ed62960
Ryan Morey <rmorey@lingraphica.com>
2020-11-15 12:09:14 -0500

    Use itertools, add update_pool

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import random
+from collections import namedtuple
+from itertools import product, permutations
+from random import choice
 
 M = 4  # code size
-N = 6  # number of colors
+N = 7  # number of colors
+REPEATS = True  # allow repeats in code
+
+
+if REPEATS:
+    POOL = tuple(product(range(N), repeat=M))
+else:
+    POOL = tuple(permutations(range(N), r=M))
+
+Response = namedtuple("Result", ("hits", "blows"))
 
 
 def check_guess(code, guess):
@@ -19,23 +30,31 @@ def check_guess(code, guess):
             code.remove(g)
             guess[i] = -2
             blows += 1
-    return hits, blows
+    return Response(hits, blows)
 
 
 def get_rand_code():
-    return tuple(random.randint(0, N - 1) for _ in range(M))
+    return choice(POOL)
 
 
 def check_rand_pair():
-    code1 = get_rand_code()
-    guess1 = get_rand_code()
-    hits, blows = check_guess(code1, guess1)
-    print(f"{code1=}")
-    print(f"{guess1=}")
+    code = get_rand_code()
+    guess = get_rand_code()
+    hits, blows = check_guess(code, guess)
+    print(f"{code=}")
+    print(f"{guess=}")
     print(f"{hits=},{blows=}")
 
 
+def get_updated_pool(pool, guess, resp):
+    return tuple(code for code in pool if check_guess(code, guess) == resp)
+
+
 if __name__ == "__main__":
-    for _ in range(10):
-        check_rand_pair()
-        print()
+    for _ in range(100):
+        code1 = get_rand_code()
+        guess1 = get_rand_code()
+        print(f"{code1=},{guess1=}")
+        new_pool = get_updated_pool(POOL, guess1, check_guess(code1, guess1))
+        print(len(new_pool))
+        assert code1 in new_pool
